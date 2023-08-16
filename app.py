60501603d10a85a97064844a7b192f37379f9a93
Fareeid <faridkim@gmail.com>
2023-08-16 23:37:10 +0300

    formatting changes

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate  # type:ignore
+from flask_wtf.csrf import CSRFProtect  # type:ignore
 from datetime import datetime
 import os
-import uuid
 
 from requests import RequestException
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder="static")
 csrf = CSRFProtect(app)
 
 # If RUNNING_IN_PRODUCTION is defined as an environment variable, then we're running on Azure
-if not 'RUNNING_IN_PRODUCTION' in os.environ:
-   # Local development, where we'll use environment variables.
-   print("Loading config.development and environment variables from .env file.")
-   app.config.from_object('azureproject.development')
+if "RUNNING_IN_PRODUCTION" not in os.environ:
+    # Local development, where we'll use environment variables.
+    print("Loading config.development and environment variables from .env file.")
+    app.config.from_object("azureproject.development")
 else:
-   # Production, we don't load environment variables from .env file but add them as environment variables in Azure.
-   print("Loading config.production.")
-   app.config.from_object('azureproject.production')
+    # Production, we don't load environment variables from .env file but add them as environment variables in Azure.  # noqa: E501
+    print("Loading config.production.")
+    app.config.from_object("azureproject.production")
 
 with app.app_context():
     app.config.update(
-        SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+        SQLALCHEMY_DATABASE_URI=app.config.get("DATABASE_URI"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
@@ -35,47 +41,60 @@ migrate = Migrate(app, db)
 
 # Create databases, if databases exists doesn't issue create
 # For schema changes, run "flask db migrate"
-from models import Restaurant, Review
+from models import Restaurant, Review  # noqa: E402, F401
+
 with app.app_context():
     db.create_all()
     db.session.commit()
 
-@app.route('/', methods=['GET'])
-def index():
-    from models import Restaurant
-    print('Request for index page received')
-    restaurants = Restaurant.query.all()    
-    return render_template('index.html', restaurants=restaurants)
 
-@app.route('/<int:id>', methods=['GET'])
+@app.route("/", methods=["GET"])
+def index():
+    from models import Restaurant  # noqa: F811
+
+    print("Request for index page received")
+    restaurants = Restaurant.query.all()
+    return render_template("index.html", restaurants=restaurants)
+
+
+@app.route("/<int:id>", methods=["GET"])
 def details(id):
-    return details(id,'')
+    return details(id, "")
+
 
 def details(id, message):
     from models import Restaurant, Review
+
     restaurant = Restaurant.query.where(Restaurant.id == id).first()
-    reviews = Review.query.where(Review.restaurant==id)
-    return render_template('details.html', restaurant=restaurant, reviews=reviews, message=message)
+    reviews = Review.query.where(Review.restaurant == id)
+    return render_template(
+        "details.html", restaurant=restaurant, reviews=reviews, message=message
+    )
 
-@app.route('/create', methods=['GET'])
+
+@app.route("/create", methods=["GET"])
 def create_restaurant():
-    print('Request for add restaurant page received')
-    return render_template('create_restaurant.html')
+    print("Request for add restaurant page received")
+    return render_template("create_restaurant.html")
 
-@app.route('/add', methods=['POST'])
+
+@app.route("/add", methods=["POST"])
 @csrf.exempt
 def add_restaurant():
-    from models import Restaurant
+    from models import Restaurant  # noqa: F811
+
     try:
-        name = request.values.get('restaurant_name')
-        street_address = request.values.get('street_address')
-        description = request.values.get('description')
-        if (name == "" or description == "" ):
+        name = request.values.get("restaurant_name")
+        street_address = request.values.get("street_address")
+        description = request.values.get("description")
+        if name == "" or description == "":
             raise RequestException()
     except (KeyError, RequestException):
         # Redisplay the restaurant entry form.
-        return render_template('create_restaurant.html', 
-            message='Restaurant not added. Include at least a restaurant name and description.')
+        return render_template(
+            "create_restaurant.html",
+            message="Restaurant not added. Include at least a restaurant name and description.",
+        )
     else:
         restaurant = Restaurant()
         restaurant.name = name
@@ -84,24 +103,29 @@ def add_restaurant():
         db.session.add(restaurant)
         db.session.commit()
 
-        return redirect(url_for('details', id=restaurant.id))
+        return redirect(url_for("details", id=restaurant.id))
 
-@app.route('/review/<int:id>', methods=['POST'])
+
+@app.route("/review/<int:id>", methods=["POST"])
 @csrf.exempt
 def add_review(id):
     from models import Review
+
     try:
-        user_name = request.values.get('user_name')
-        rating = request.values.get('rating')
-        review_text = request.values.get('review_text')   
-        if (user_name == "" or rating == None):
+        user_name = request.values.get("user_name")
+        rating = request.values.get("rating")
+        review_text = request.values.get("review_text")
+        if user_name == "" or rating == None:
             raise RequestException()
     except (KeyError, RequestException):
         # Redisplay the review form.
         from models import Restaurant
+
         restaurant = Restaurant.query.where(Restaurant.id == id).first()
-        reviews = Review.query.where(Review.restaurant==id)
-        return details(id, 'Review not added. Include at least a name and rating for review.')
+        reviews = Review.query.where(Review.restaurant == id)
+        return details(
+            id, "Review not added. Include at least a name and rating for review."
+        )
     else:
         review = Review()
         review.restaurant = id
@@ -111,31 +135,42 @@ def add_review(id):
         review.review_text = review_text
         db.session.add(review)
         db.session.commit()
-                
-    return redirect(url_for('details', id=id))        
+
+    return redirect(url_for("details", id=id))
+
 
 @app.context_processor
 def utility_processor():
     def star_rating(id):
         from models import Review
-        reviews = Review.query.where(Review.restaurant==id)
+
+        reviews = Review.query.where(Review.restaurant == id)
 
         ratings = []
-        review_count = 0;        
+        review_count = 0
         for review in reviews:
             ratings += [review.rating]
             review_count += 1
 
-        avg_rating = round(sum(ratings)/len(ratings), 2) if ratings else 0
+        avg_rating = round(sum(ratings) / len(ratings), 2) if ratings else 0
         stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
-        return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
+        return {
+            "avg_rating": avg_rating,
+            "review_count": review_count,
+            "stars_percent": stars_percent,
+        }
 
     return dict(star_rating=star_rating)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-if __name__ == '__main__':
-   app.run()
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+if __name__ == "__main__":
+    app.run()
